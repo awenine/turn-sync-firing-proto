d89ae2146d33ef73d5bbb6757088ebd457b1160c
Chris Owen <c.ornine@gmail.com>
2025-06-18 21:23:32 +0700

    feat: instantiate hit marker at collision point

--- player_02.gd
+++ player_02.gd
@@ -1,6 +1,7 @@
 extends MeshInstance3D
 
 @onready var ray_cast_3d: RayCast3D = $StaticBody3D/CollisionShape3D/RayCast3D
+var hit_marker = preload("res://hit_marker.tscn")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
@@ -8,4 +9,11 @@ func _input(event: InputEvent) -> void:
 		print("SHOOT") 
 		print(ray_cast_3d)
 		print(ray_cast_3d.get_collider())
+		shoot_ray()
 		#RayCast.target_position = 
+
+
+func shoot_ray() -> void:
+	var instance = hit_marker.instantiate()
+	instance.set_position(ray_cast_3d.get_collision_point())	
+	get_parent_node_3d().add_child(instance)
