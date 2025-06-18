f9d7e59de855508544c95072882db4798d201baa
Chris Owen <c.ornine@gmail.com>
2025-06-18 23:30:40 +0700

    feat: make both players shoot

--- player_02.gd
+++ player_02.gd
@@ -1,24 +1,30 @@
 extends MeshInstance3D
 
 @onready var ray_cast_3d: RayCast3D = $StaticBody3D/CollisionShape3D/RayCast3D
+
+
 var hit_marker = preload("res://hit_marker.tscn")
 
-const spread_factor = 6
+const spread_factor = 22
 
+func _ready() -> void:
+#	Running first to make sure initial shot isnt always in same place
+	get_new_ray_direction()
+	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
 		# do something
 		print("SHOOT") 
-		print(ray_cast_3d)
-		print(ray_cast_3d.get_collider())
+		get_new_ray_direction()
 		shoot_ray()
 		#RayCast.target_position = 
 
 
 func shoot_ray() -> void:
-	get_new_ray_direction()
 	var instance = hit_marker.instantiate()
-	instance.set_position(ray_cast_3d.get_collision_point())	
+	var collision_point = ray_cast_3d.get_collision_point()
+	print(collision_point)
+	instance.set_position(collision_point)	
 	get_parent_node_3d().add_child(instance)
 
 func get_new_ray_direction() -> void:
