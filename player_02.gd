f1265fb04f2128e5421b3a0246d36f66fd0e9639
Chris Owen <c.ornine@gmail.com>
2025-06-18 21:43:57 +0700

    feat: randomize ray at firing time

--- player_02.gd
+++ player_02.gd
@@ -3,6 +3,8 @@ extends MeshInstance3D
 @onready var ray_cast_3d: RayCast3D = $StaticBody3D/CollisionShape3D/RayCast3D
 var hit_marker = preload("res://hit_marker.tscn")
 
+const spread_factor = 6
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
 		# do something
@@ -14,6 +16,12 @@ func _input(event: InputEvent) -> void:
 
 
 func shoot_ray() -> void:
+	get_new_ray_direction()
 	var instance = hit_marker.instantiate()
 	instance.set_position(ray_cast_3d.get_collision_point())	
 	get_parent_node_3d().add_child(instance)
+
+func get_new_ray_direction() -> void:
+	#	for now keep forward ray the same
+	ray_cast_3d.target_position.x = (randf()-0.5) * 2 * spread_factor
+	ray_cast_3d.target_position.y = (randf()-0.5) * 2 * spread_factor
