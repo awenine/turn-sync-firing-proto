9a55b79010503bf9643d8bb45212c033668b3456
Chris Owen <c.ornine@gmail.com>
2025-06-20 22:01:35 +0700

    feat: add bullet colour change and preview update to player

--- player.gd
+++ player.gd
@@ -1,3 +1,4 @@
+@tool
 extends MeshInstance3D
 
 @onready var ray_cast_3d: RayCast3D = $StaticBody3D/CollisionShape3D/RayCast3D
@@ -5,8 +6,19 @@ extends MeshInstance3D
 
 var hit_marker = preload("res://hit_marker.tscn")
 
-const spread_factor = 22
+@export var spread_factor = 13
+	#set(value):
+		#spread_factor = value
+		#get_new_ray_direction()
+		
+#@export_color_no_alpha var player_color:
+	#set(value):
+		#player_color = value
+		#get_mesh().surface_get_material(0).albedo_color = value
+		
+@export var bullet_material: StandardMaterial3D
 
+		
 func _ready() -> void:
 #	Running first to make sure initial shot isnt always in same place
 	get_new_ray_direction()
@@ -22,6 +34,8 @@ func _input(event: InputEvent) -> void:
 
 func shoot_ray() -> void:
 	var instance = hit_marker.instantiate()
+	# using get_child here as a stand in, should make material setting function in the hhit marker for more flexibility	
+	instance.get_child(0).set_surface_override_material(0, bullet_material)
 	var collision_point = ray_cast_3d.get_collision_point()
 	print(collision_point)
 	instance.set_position(collision_point)	
